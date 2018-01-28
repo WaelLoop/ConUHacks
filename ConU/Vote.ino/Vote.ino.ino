7678fee85a210e9eaca022824385d312b0a97a6c
affanjilani <obiaffan@gmail.com>
2018-01-27 21:20:28 -0500

    Finished vote.ino

--- ConU/Vote.ino/Vote.ino.ino
+++ ConU/Vote.ino/Vote.ino.ino
@@ -19,6 +19,18 @@ String name1;
 int choice2 = 0;
 String name2;
 
+const int startPin = 2;
+const int whitePin = 3; //choice 1
+const int bluePin = 4;  //choice 2
+
+//variables will change
+int startButtonState = 0;
+int whiteButtonState = 0;
+int blueButtonState = 0;
+
+//variable waiting for a button to be pressed in vote()
+boolean stat = true;
+
 //only runs once at the start
 void setup() {
   
@@ -45,9 +57,12 @@ void setup() {
     while (1) { delay(1); }
   } 
 
-  /*
-   * TODO: Set up buttons
-   */
+  //initializing the button inputs
+  pinMode(startPin, INPUT);
+
+  pinMode(bluePin, INPUT);
+
+  pinMode(whitePin, INPUT);
 
 }
 
@@ -55,9 +70,10 @@ void setup() {
 void loop() {
   Serial.println("\nPlease press start to vote.");
 
-  /*
-   * get button input for start and put here
-   */
+  //get button input for start and put here
+   do{
+     startButtonState = digitalRead(startPin);
+   }while(startButtonState != HIGH);
 
   //get slot of finger scanned, if not enrolled returns 0, -1 if error, slot if good
   int inSlot = getFinger();
@@ -77,12 +93,13 @@ void loop() {
     else if(inSlot==0){
       int newSlot = enroll();
       if(newSlot){
-        Serial.println("Finger successfully enroled");
+        Serial.println("Finger successfully enrolled");
         vote(newSlot);
       }
+      else if(newSlot == 0) Serial.println("Not enough space in scanner for new enrollment!");
       else Serial.println("Could not enroll, try again");
     }
-    //there was an error, so print it out and then
+    //there was an error
     else{
       Serial.println("We are experiencing technical and spiritual difficulties :( Please try again.");
     }
@@ -92,16 +109,156 @@ void loop() {
 /*
  * enroll
  * starts the process to enroll a finger that was not found.
- * returns slot number if successful, or 0 if there was an error
+ * returns slot number if successful, or -1 if there was an error, 0 if no space
  */
 int enroll(){
 
   //get current amount of fingers in scanner
   finger.getTemplateCount();
-  int numberOfEntries = finger.templateCount;
+  int numberOfEntries = (int) finger.templateCount;
 
-  if
+  //make sure we don't go over maximum capacity
+  if(numberOfEntries>=127) return 0;
+
+  //add to next entry
+  int id = numberOfEntries++;
+
+  //start enrollment process
+  int p = -1;
+  Serial.println("Please place your right thumb on the scanner for enrollment.");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.println(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
   
+  //create template from the picture
+  p = finger.image2Tz(1);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return -1;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return -1;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return -1;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return -1;
+    default:
+      Serial.println("Unknown error");
+      return -1;
+   }
+
+  //Make them remove finger and then confirm with same finger
+  Serial.println("Remove finger");
+  delay(2000);
+  p = 0;
+  while (p != FINGERPRINT_NOFINGER) {
+    p = finger.getImage();
+  }
+
+  p = -1;
+  Serial.println("Place same finger again");
+  while (p != FINGERPRINT_OK) {
+    p = finger.getImage();
+    switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image taken");
+      break;
+    case FINGERPRINT_NOFINGER:
+      Serial.print(".");
+      break;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      break;
+    case FINGERPRINT_IMAGEFAIL:
+      Serial.println("Imaging error");
+      break;
+    default:
+      Serial.println("Unknown error");
+      break;
+    }
+  }
+
+  //make second template
+  p = finger.image2Tz(2);
+  switch (p) {
+    case FINGERPRINT_OK:
+      Serial.println("Image converted");
+      break;
+    case FINGERPRINT_IMAGEMESS:
+      Serial.println("Image too messy");
+      return -1;
+    case FINGERPRINT_PACKETRECIEVEERR:
+      Serial.println("Communication error");
+      return -1;
+    case FINGERPRINT_FEATUREFAIL:
+      Serial.println("Could not find fingerprint features");
+      return -1;
+    case FINGERPRINT_INVALIDIMAGE:
+      Serial.println("Could not find fingerprint features");
+      return -1;
+    default:
+      Serial.println("Unknown error");
+      return -1;
+  }  
+
+  //create model
+  p = finger.createModel();
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Prints matched!");
+  } 
+  else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return -1;
+  } 
+  else if (p == FINGERPRINT_ENROLLMISMATCH) {
+    Serial.println("Fingerprints did not match");
+    return -1;
+  } 
+  else {
+    Serial.println("Unknown error");
+    return -1;
+  }
+
+  //store model into scanner
+  p = finger.storeModel(id);
+  if (p == FINGERPRINT_OK) {
+    Serial.println("Stored!");
+  } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
+    Serial.println("Communication error");
+    return -1;
+  } else if (p == FINGERPRINT_BADLOCATION) {
+    Serial.println("Could not store in that location");
+    return -1;
+  } else if (p == FINGERPRINT_FLASHERR) {
+    Serial.println("Error writing to flash");
+    return -1;
+  } else {
+    Serial.println("Unknown error");
+    return -1;
+  }   
 }
 
 /*
@@ -109,14 +266,29 @@ int enroll(){
  * records a vote for one candidate or another, increments the total, and marks the id as voted
  */
 void vote(int voterID){
-  Serial.println("Please cast a vote for your candidate.")
-  /*TODO: check for user press
+  Serial.println("Please cast a vote for your candidate.");
+  /* check for user press
    * confirm vote casting
    * increment global variable for candidate depending on choice
    */
+   while(stat != false){
+    whiteButtonState = digitalRead(whitePin);
+    blueButtonState = digitalRead(bluePin);
+
+    if(whiteButtonState == HIGH){
+      stat = false;
+      choice1++;
+    }
+    if(blueButtonState == HIGH){
+      stat = false;
+      choice2++;
+    }
+  }
 
   //update voting status of voter
   voters[voterID] = 1; 
+
+  Serial.println("Your vote was casted!");
 }
 
 /*
@@ -126,7 +298,7 @@ void vote(int voterID){
  * Returns boolean true if and only if a voter can vote
  */
 boolean isEligible(int voterID){
-  return voters[voterID] == 0
+  return voters[voterID] == 0;
 }
 
 /*
